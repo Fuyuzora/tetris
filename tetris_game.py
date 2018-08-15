360101201a459c335e124ca29bc5edff84d617e9
Fuyuzora <rwan7922@gmail.com>
2018-08-14 21:40:14 -0400

    debugging

--- tetris_game.py
+++ tetris_game.py
@@ -1,12 +1,166 @@
 import sys, random
-from PyQt5.QtWidgets import QFrame, QMainWindow, QApplication, QDesktopWidget
-from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication, QHBoxLayout, QLabel
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
+from PyQt5.QtGui import QPainter, QColor
 
-class Tetris(QFrame):
+from tetris_shape import Shape, BOARD_DATA
+
+class Board(QFrame):
+    # msg2Statusbar = pyqtSignal(str)
+    speed = 1000
+
+    def __init__(self, parent, gridSize):
+        super().__init__(parent)
+        self.setFixedSize(gridSize * BOARD_DATA.width, gridSize * BOARD_DATA.height)
+        self.gridSize = gridSize
+        self.initBoard()
+
+    def initBoard(self):
+        print('Board initiated')
+        self.score = 0
+        BOARD_DATA.clear()
+    
+    def paintEvent(self, event):
+        print('printEvent triggered')
+        painter = QPainter(self)
+        # painter.begin()
+
+        # draw backboard
+        for x in range(BOARD_DATA.width):
+            for y in range (BOARD_DATA.height):
+                val = BOARD_DATA.getValue(x, y)
+                drawSquare(painter, x * self.gridSize, y * self.gridSize, val, self.gridSize)
+        # draw current shape
+        for x, y in BOARD_DATA.getCurShapeCoord():
+            val = BOARD_DATA.curShape.shape
+            drawSquare(painter, x * self.gridSize, y * self.gridSize, val, self.gridSize)
+
+        # draw the border
+        painter.setPen(QColor(0x777777))
+        painter.drawLine(self.width() - 1, 0, self.width() - 1, self.height())
+        painter.setPen(QColor(0xcccccc))
+        painter.drawLine(self.width(), 0, self.width(), self.height())
+
+        # painter.end()
+    
+    def updateData(self):
+        print('Board updated')
+        # self.msg2Statusbar.emit(str(self.
+        # 1score))
+        self.update()
+
+class Tetris(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.isStarted = False
+        self.isPaused = False
+        self.nextMove = None
+        self.lastShape = Shape.shapeNone
 
+        self.initUI()
+    
     def initUI(self):
+        self.gridSize = 22
+        self.speed = 500
+
+        self.timer = QBasicTimer()
+        self.setFocusPolicy(Qt.StrongFocus)
+
+        hbox = QHBoxLayout()
+        self.tboard = Board(self, self.gridSize)
+        hbox.addWidget = self.tboard
+
+        self.statusbar = self.statusBar()
+        # self.tboard.msg2Statusbar[str].connect(self.statusbar.showMessage)
+
+        self.start()
+
+        self.center()
+        self.setWindowTitle('Tetris')
+        self.show()
+        self.setFixedSize(self.tboard.width(), 500 + self.statusbar.height())
+    
+    def center(self):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width() - size.width()) // 2 , (screen.height() - size.height()) // 2)
+    
+    def start(self):
+        self.isStarted = True
+        self.tboard.score = 0
+        BOARD_DATA.clear()
+
+        # self.tboard.msg2Statusbar.emit(str(self.tboard.score))
+
+        BOARD_DATA.createNewPiece()
+        self.timer.start(self.speed, self)
+    
+    def updateWindow(self):
+        self.tboard.updateData()
+        self.update()
+
+    def timerEvent(self, event):
+        if event.timerId() == self.timer.timerId():
+            if self.nextMove:
+                k = 0
+                while BOARD_DATA.curDirection != self.nextMove[0] and k < 4:
+                    BOARD_DATA.rotateRight()
+                    k += 1
+                k = 0
+                while BOARD_DATA.curX != self.nextMove[1] and k < 5:
+                    if BOARD_DATA.curX > self.nextMove[1]:
+                        BOARD_DATA.moveLeft()
+                    elif BOARD_DATA.curX < self.nextMove[1]:
+                        BOARD_DATA.moveRight()
+                    k += 1
+            lines = BOARD_DATA.moveDown()
+            self.tboard.score += lines
+            if self.lastShape != BOARD_DATA.curShape:
+                self.nextMove = None
+                self.lastShape = BOARD_DATA.curShape
+            self.updateWindow()
+        else:
+            super(Tetris, self).timerEvent(event)
+
+    def keyPressEvent(self, event):
+        if not self.isStarted or BOARD_DATA.curShape == Shape.shapeNone:
+            super(Tetris, self).keyPressEvent(event) 
+            return
+        key = event.key()
+
+        if key == Qt.Key_Left:
+            BOARD_DATA.moveLeft()
+        elif key == Qt.Key_Right:
+            BOARD_DATA.moveRight()
+        elif key == Qt.Key_Up:
+            BOARD_DATA.rotateLeft()
+        elif key == Qt.Key_Down:
+            BOARD_DATA.rotateRight()
+        else:
+            super(Tetris, self).keyPressEvent(event)
         
+        self.updateWindow()
+    
+def drawSquare(painter, x, y, val, s):
+    colorTable = [0x000000, 0xCC6666, 0x66CC66, 0x6666CC,
+                  0xCCCC66, 0xCC66CC, 0x66CCCC, 0xDAAA00, 0xFFFFFF]
+    if val == 0:
+        return
+        
+    color = QColor(colorTable[val])
+    painter.fillRect(x + 1, y + 1, s - 2, s - 2, color)
+
+    painter.setPen(color.lighter())
+    painter.drawLine(x, y + s - 1, x, y)
+    painter.drawLine(x, y, x + s - 1, y)
+
+    painter.setPen(color.darker())
+    painter.drawLine(x + 1, y + s - 1, x + s - 1, y + s - 1)
+    painter.drawLine(x + s - 1, y + s - 1, x + s - 1, y + 1)
+
+
+if __name__ == '__main__':
+    # random.seed(32)
+    app = QApplication([])
+    tetris = Tetris()
+    sys.exit(app.exec_())
--- tetris_shape.py
+++ tetris_shape.py
@@ -62,7 +62,7 @@ class Shape(object):
                 minY = y
             if maxY > y:
                 maxY = y
-        return (minx, maxx, minY, maxY)
+        return (minX, maxX, minY, maxY)
 
 class BoardData(object):
     width = 10
@@ -77,8 +77,108 @@ class BoardData(object):
         self.curShape = Shape()
         self.nextShape = Shape(random.randint(1, 7))
 
+        self.shapeStatus = [0] * 8
+
     def getData(self):
         return self.backBoard[:]
     
     def getValue(self, x, y):
         return self.backBoard[x + y * BoardData.width]
+    
+    def getCurShapeCoord(self):
+        return self.curShape.getCoords(self.curDirection, self.curX, self.curY)
+    
+    def tryMove(self, shape, direction, x, y):
+        for x, y in shape.getCoords(direction, x, y):
+            if x >= BoardData.width or x < 0 or y >= BoardData.height or y < 0:
+                return False
+            if self.backBoard[x + y * BoardData.width > 0]:
+                return False
+        return True
+    
+    def tryMoveCurrent(self, direction, x, y):
+        return self.tryMove(self.curShape, direction, x, y)
+    
+    def createNewPiece(self):
+        minX, maxX, minY, maxY = self.nextShape.getBoundingOffsets(0)
+        result = False
+        if self.tryMoveCurrent(0, self.width / 2, -minY):
+            print('getting new piece')
+            self.curX = self.width / 2
+            self.curY = -minY
+            self.curDirection = 0
+            self.curShape = self.nextShape
+            self.nextShape = Shape(random.randint(1, 7))
+            result = True
+        else:
+            print('you died')
+            self.curShape = Shape()
+            self.curX = -1
+            self.curY = -1
+            self.curDirection = 0
+            result = False
+        self.shapeStatus[self.curShape.shape] += 1
+        return result 
+
+    def moveDown(self):
+        lines = 0
+        # print(self.tryMoveCurrent(self.curDirection, self.curX, self.curY + 1))
+        if self.tryMoveCurrent(self.curDirection, self.curX, self.curY + 1):
+            self.curY += 1
+        else:
+            self.mergePiece()
+            lines = self.removeFilledLines()
+            self.createNewPiece()
+        return lines
+    
+    def moveLeft(self):
+        if self.tryMoveCurrent(self.curDirection, self.curX - 1, self.curY):
+            self.curX -= 1
+    
+    def moveRight(self):
+        if self.tryMoveCurrent(self.curDirection, self.curX + 1, self.curY):
+            self.curX += 1
+    
+    def rotateRight(self):
+        if self.tryMoveCurrent((self.curDirection + 1) % 4, self.curX, self.curY):
+            self.curDirection += 1
+            self.curDirection %= 4
+
+    def rotateLeft(self):
+        if self.tryMoveCurrent((self.curDirection - 1) % 4, self.curX, self.curY):
+            self.curDirection -= 1
+            self.curDirection %= 4
+
+    def removeFilledLines(self):
+        newBackBoard = [0] * BoardData.height * BoardData.width
+        newY = BoardData.height - 1
+        lines = 0
+        for y in range (BoardData.height - 1, -1, -1):
+            blockCount = sum([1 if self.backBoard[x + y * BoardData.width] > 0 else 0 for x in range(BoardData.width)])
+            if blockCount < BoardData.width:
+                for x in range(BoardData.width):
+                    newBackBoard[x + newY * BoardData.width] = self.backBoard[x + y * BoardData.width]
+                newY -= 1
+            else:
+                lines += 1
+        if lines > 0:
+            self.backBoard = newBackBoard
+        return lines
+
+    def mergePiece(self):
+        for x, y in self.curShape.getCoords(self.curDirection, self.curX, self.curY):
+            self.backBoard[int(x + y * BoardData.width)] = self.curShape.shape
+        
+        self.curX = -1
+        self.curY = -1
+        self.curDirection = 0
+        self.curShape = Shape()
+    
+    def clear(self):
+        self.curX = -1
+        self.curY = -1
+        self.curDirection = 0
+        self.curShape = Shape()
+        self.backBoard = [0] * BoardData.width * BoardData.height
+
+BOARD_DATA = BoardData()
